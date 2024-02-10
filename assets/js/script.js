739c9bdce0f7841dba04f6919192eb6b38092b17
Lavken <lavkenpilar@gmail.com>
2024-02-10 01:38:38 -0300

    create-index-js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const bancos = ["Estado","Santander","BCI", "ITAU", "Scotiabank" ]
+console.log (bancos [1])
+
+const colores =["azúl", "Verde", "Morado", "Rojo", "Blanco", "Amarillo"]
+let longitudDelArreglo = colores.length 
+let ultimoIndice = longitudDelArreglo -1
+console.log (colores.length)
+console.log(longitudDelArreglo -1)
+console.log (colores[ultimoIndice])
